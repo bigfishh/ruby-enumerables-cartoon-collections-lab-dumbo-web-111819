0139dd95d1af727a07bae8272eb8a6a50768e4db
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 05:12:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_array)
-  if calls_array.any?{|words| words.length < 4}
+  if calls_array.any?{|words| words.length > 4}
     return true 
   else 
     return false 
