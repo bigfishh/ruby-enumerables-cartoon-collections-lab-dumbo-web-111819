895b85961f2affa9964940d4385ce1df85911fe2
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 05:08:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls_array)
-  if !calls_array.any?{|words| words.length < 4}
-    return false 
-  end
+  calls_array.any?{|words| words.length < 4}
 end
 
 def find_the_cheese# code an argument here
